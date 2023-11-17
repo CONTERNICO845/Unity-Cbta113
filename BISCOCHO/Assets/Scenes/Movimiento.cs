1a36e990e3f33807fc93972a866d45c47fc349ad
CONTERNICO845 <geovanigaelcb@gmail.com>
2023-11-17 09:20:31 -0600

    Nuevo Movimiento

--- BISCOCHO/Assets/Scenes/Movimiento.cs
+++ BISCOCHO/Assets/Scenes/Movimiento.cs
@@ -1,26 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SearchService;
 
-public class Movimiento : MonoBehaviour {
-    public float force = 90909;
+public class Movimiento : MonoBehaviour
+{
+    public float force = 150;
     Rigidbody rb;
+    Camera mainCamera;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        
+        mainCamera = Camera.main; // Assuming your camera is tagged as "MainCamera" or set as the main camera
     }
 
-    // Update is called once per frame
-    void Update()
+    // FixedUpdate is called once per physics update
+    void FixedUpdate()
     {
         float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("vertical");
-        
-        Vector3 vector = new Vector3(h, 0.5f, v);
+        float v = Input.GetAxis("Vertical");
 
-        rb.AddForce(vector*force*Time.deltaTime);
+        Vector3 cameraForward = mainCamera.transform.forward;
+        Vector3 cameraRight = mainCamera.transform.right;
+
+        cameraForward.y = 0f;
+        cameraRight.y = 0f;
+
+        Vector3 movement = (cameraForward.normalized * v + cameraRight.normalized * h).normalized;
+
+        rb.AddForce(movement * force * Time.fixedDeltaTime);
     }
 }

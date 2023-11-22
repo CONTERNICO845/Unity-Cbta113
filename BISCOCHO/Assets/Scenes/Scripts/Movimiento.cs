b38d4a14aa1c8155de75292530f4f56016ab865e
ELTUTAUSENMADAFAKA <151105081+ELTUTAUSENMADAFAKA@users.noreply.github.com>
2023-11-21 18:46:11 -0600

    camara 50/50 de movimiento

--- BISCOCHO/Assets/Scenes/Scripts/Movimiento.cs
+++ BISCOCHO/Assets/Scenes/Scripts/Movimiento.cs
@@ -2,21 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Movimiento : MonoBehaviour
 {
+    private new Transform camera;
+    public Vector2 sensivility; 
     // Start is called before the first frame update
     void Start()
     {
+        camera = transform.Find("Main Camera");
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float hor = Input.GetAxis("Mause X");
-        if (hor > 0)
+        float hor = Input.GetAxis("Mouse X");
+        float ver = Input.GetAxis("Mouse Y");
+        if (hor != 0)
         {
-            transform.Rotate(Vector3.up * hor);
+            transform.Rotate(Vector3.up * hor * sensivility.x);
+
+        }
+        if (ver != 0)
+        {
+            //transform.Rotate(Vector3.left * ver * sensivility.y);
+            float angle = (camera.localEulerAngles.x - ver * sensivility.y) % 360;
+            if (angle > 180) { angle -= 360; }
+            angle = Mathf.Clamp(angle, -80, +80);
+            camera.localEulerAngles=Vector3.right * angle;
         }
     }
 }

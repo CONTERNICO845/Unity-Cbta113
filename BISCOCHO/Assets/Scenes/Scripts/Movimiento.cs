c5f8baf8035425ea81a75ff8e429f0574b2b18e9
ELTUTAUSENMADAFAKA <151105081+ELTUTAUSENMADAFAKA@users.noreply.github.com>
2023-11-21 18:09:56 -0600

    skri

--- BISCOCHO/Assets/Scenes/Scripts/Movimiento.cs
+++ BISCOCHO/Assets/Scenes/Scripts/Movimiento.cs
@@ -7,12 +7,16 @@ public class Movimiento : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float hor = Input.GetAxis("Mause X");
+        if (hor > 0)
+        {
+            transform.Rotate(Vector3.up * hor);
+        }
     }
 }
